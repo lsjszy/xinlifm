8f2889df861fda9ffdb2fb902a795599fe0930c1
asdfghjkl192 <1923790383@qq.com>
2021-11-01 21:49:40 +0800

    页面大致

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,50 @@
-// index.js
 // 获取应用实例
-const app = getApp()
+const app = getApp();
+import { requestGet,SlidesURL } from "../../utils/reqeust";
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    slides: [],
+    
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  
+    onLoad() {
+    this.getSlidesData();
   },
-  onLoad() {
-    if (wx.getUserProfile) {
+  async getSlidesData() {
+    const result = await requestGet(SlidesURL);
+    this.setData({
+      slides: result.data,
+    });
+  },
+
+   // 获取滚动条当前位置
+  onPageScroll: function (e) {
+    // console.log(e) 自己设置 滚动距离,200
+    if (e.scrollTop > 200) {
       this.setData({
-        canIUseGetUserProfile: true
+        floorstatus: true
+      });
+    } else {
+      this.setData({
+        floorstatus: false
+      });
+    }
+  },
+
+  //回到顶部
+  goTop: function (e) {  // 一键回到顶部
+    if (wx.pageScrollTo) {
+      wx.pageScrollTo({
+        scrollTop: 0
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
       })
     }
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
+});
+
+
---
+++ utils/reqeust.js
@@ -0,0 +1,36 @@
+export function requestGet(url, data) {
+  return new Promise((reslove, reject) => {
+    wx.request({
+      //请求地址
+      url: url,
+      //请求方式
+      method: "get",
+      //请求参数
+      data: data,
+      //设置请求头  如果发送的是post请求，一定要添加请求的content-type
+      header: {
+        "content-type": "application/json",
+      },
+      //请求返回结果的数据类型
+      dataType: "json",
+      //请求回调
+      success: ({ statusCode, data }) => {
+        if (statusCode === 200) {
+          reslove(data);
+        } else {
+          reject("服务器响应出错");
+        }
+      },
+      // 请求失败执行的回调函数
+      fail: function (err) {
+        reject(err)
+      },
+      // 接口调用结束的回调函数（调用成功、失败都会执行）
+      complete: function (res) {},
+    });
+  });
+}
+
+
+export var SlidesURL = "https://bapi.xinli001.com/fm2/broadcast_list.json/?offset=0&speaker_id=0&tag=%E6%8A%91%E9%83%81%E7%97%87%E6%98%AF%E6%9D%A1%E9%BB%91%E7%8B%97&rows=10&key=046b6a2a43dc6ff6e770255f57328f89"//GET请求
+
